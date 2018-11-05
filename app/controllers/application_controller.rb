0de5f547e15a91b0c2811587178a08b42eda0f84
samira <s.soltani336@gmail.com>
2018-11-05 21:55:03 +0330

    Add admin role and redirect the admin users to the admin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+    protected
+
+    def after_sign_in_path_for(resource) 
+        return admin_path  if resource.is_admin
+        return root_url
+    end
 end
