e307499040b7ffb0d10ee8c1195b46317d6fcb1b
samira <s.soltani336@gmail.com>
2018-11-05 21:10:27 +0330

    Add root and custom routes for login, logout and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, :skip => [:sessions, :registrations]
+  
+
+  devise_scope :user do
+     root to: "home#home"
+     get    "login",  to: "devise/sessions#new",         as: :new_user_session
+     post   "login",  to: "devise/sessions#create",      as: :user_session
+     delete "logout", to: "devise/sessions#destroy",     as: :destroy_user_session
+ 
+     get    "sign-up",  to: "devise/registrations#new",    as: :new_user_registration
+     post   "sign-up",  to: "devise/registrations#create", as: :user_registration
+  end
+
+ get "admin", to: "admin#index"
+
 end
