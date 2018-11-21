40556d22bdfc78698844334fb1fc47f53169965d
samira <s.soltani336@gmail.com>
2018-11-21 20:43:44 +0330

    Add validations for series and videos models

--- app/models/series.rb
+++ app/models/series.rb
@@ -1,3 +1,6 @@
 class Series  < ApplicationRecord
     has_many :videos
+    validates :title, presence: true
+    validates :level, presence: true
+    validates :description, presence: true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ApplicationRecord
-    belongs_to :series
+    belongs_to :series 
+    validates :title, presence: true  
+    validates :url, presence: true
+    validates :description, presence: true 
+    validates_format_of :url, :with => URI::regexp(%w(http https))
 end
