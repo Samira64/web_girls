0a1d6c80d4a7ab299d015e0d8446afe238fa0af7
samira <s.soltani336@gmail.com>
2018-11-27 11:10:34 +0330

    Format series index and users table files

--- db/migrate/20181010191819_devise_create_users.rb
+++ db/migrate/20181010191819_devise_create_users.rb
@@ -32,8 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
        t.string   :unlock_token # Only if unlock strategy is :email or :both
        t.datetime :locked_at
 
-
-      t.timestamps null: false
+       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
