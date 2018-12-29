36a8c0548ce159848de3935904d694b781490d61
samira <s.soltani336@gmail.com>
2018-12-29 16:03:57 +0330

    Delete associated tags on video destroy

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ApplicationRecord
     belongs_to :series 
     has_and_belongs_to_many :tags
+    accepts_nested_attributes_for :tags, allow_destroy: true
     
     validates :title, presence: true  
     validates :url, presence: true
