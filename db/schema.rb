44866771149e87194987b512c473c86530e02c54
samira <s.soltani336@gmail.com>
2018-12-29 15:07:04 +0330

    Create tags table and add a vaidation for 'name' attribute

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_084658) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
